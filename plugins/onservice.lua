ac22a478155860f19ba9835941cf5612ef830cac
silver-des <ramtin.ataei20@gmail.com>
2016-01-01 10:56:42 +0330

    Update onservice.lua

--- plugins/onservice.lua
+++ plugins/onservice.lua
@@ -1,18 +1,1 @@
-do
--- Will leave the group if be added
-local function run(msg)
-local bot_id = our_id -- your bot id
-   -- like local bot_id = 1234567
-    if msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) then
-       send_large_msg("chat#id"..msg.to.id, 'this is not one of my groups.', ok_cb, false)
-       chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
-	end
-end
 
-return {
-  patterns = {
-	"^!!tgservice (.+)$",
-  },
-  run = run
-}
-end
